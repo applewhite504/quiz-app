20d8a09f01926ed887b3b806a9e8ad44f48e0fea
Beth Applewhite <bethapplewhite.nola@gmail.com>
2019-12-06 13:41:12 -0600

    files added by mistatke

--- images/Thank you for your purchase! - Canvastry - Checkout_files/replay.js
+++
@@ -1,7 +0,0 @@
-
-(function(w,d,n,u,o,t,m){w['SrecObject']=o;w[o]=w[o]||function(){
-(w[o].q=w[o].q||[]).push(arguments)},w[o].l=1*new Date();t=d.createElement(n),
-m=d.getElementsByTagName(n)[0];t.async=1;t.src=u;m.parentNode.insertBefore(t,m)
-})(window,document,'script','https://replayapp.tk/collect/initialize.js','srec');
-srec('init', '804a3510-0180-11ea-9e07-3dba8d1a0bfa');
-    
